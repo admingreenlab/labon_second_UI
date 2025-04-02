66983f9d684c511c3d9c02a12b5365dddaecb88a
kishanmodi5 <kishan.modi.greenlab@gmail.com>
2025-04-02 17:02:08 +0530

    new update

--- app/route-change-listener.js
+++ app/route-change-listener.js
@@ -3,11 +3,12 @@
 import { usePathname } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 import { SearchContext } from "@/components/context/SearchContext";
-import { PolishProvider } from "@/components/context/PolishContext"; 
+import { PolishContext } from "@/components/context/PolishContext";
 
 
 export function RouteChangeListener() {
     const { clearSearchState } = useContext(SearchContext);
+    const { clearPolishState } = useContext(PolishContext);
     const pathname = usePathname();
     const [changes, setChanges] = useState(0);
 
@@ -17,6 +18,17 @@ export function RouteChangeListener() {
             console.log('not cleaer')
         }else{
             clearSearchState();
+            console.log('cleared')
+        }
+        setChanges((prev) => prev + 1);
+    }, [pathname]);
+
+    useEffect(() => {
+        console.log(`Route changed to: ${pathname}`);
+        if(pathname.startsWith('/polish')){
+            console.log('not cleaer')
+        }else{
+            clearPolishState();
             console.log('cleared')
         }
         setChanges((prev) => prev + 1);
